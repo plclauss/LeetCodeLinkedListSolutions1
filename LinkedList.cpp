298855be23bd18b21e861235a6dd956e896e2ca5
Paul Clauss <plclauss@gmail.com>
2022-04-15 20:46:37 -0500

    Finished second problem

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -18,3 +18,25 @@ ListNode* deleteDuplicates(ListNode* head) {
 //    free(headCopy);
     return head;
 }
+
+bool hasCycle(ListNode* head) {
+    // KEY = address of node
+    // MAPPED = position
+    std::unordered_map<ListNode*, int> map;
+    int32_t pos = -1;
+
+    ListNode* headCopy = head;
+
+    uint32_t index = 0;
+    while (headCopy) {
+        map.insert(std::make_pair(headCopy, index));
+        if (map.find(headCopy->next) != map.end()) {
+            pos = map.find(headCopy->next)->second;
+            return map.find(headCopy->next) != map.end();
+        }
+        headCopy = headCopy->next;
+        index++;
+    }
+
+    return false;
+}
